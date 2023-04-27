5341bd0aa63db785f68798d581c37d7abacd30bf
Pawel Miniszewski <pawel.miniszewski@gmail.com>
2023-04-27 19:23:43 +0200

    simplify code

--- rxcpp-msvc-error/main.cpp
+++ rxcpp-msvc-error/main.cpp
@@ -2,25 +2,11 @@
 
 int main()
 {
-    auto obs1 = rxcpp::observable<>::create<int>([](rxcpp::subscriber<int> _sub) {
-        _sub.on_next(1);
-    _sub.on_next(2);
-    _sub.on_next(3);
-    _sub.on_completed();
-        });
+    auto obs1 = rxcpp::observable<>::just(1);
+    auto obs2 = rxcpp::observable<>::just(2);
 
-    auto obs2 = rxcpp::observable<>::create<int>([](rxcpp::subscriber<int> _sub) {
-        _sub.on_next(98);
-    _sub.on_next(99);
-    _sub.on_next(100);
-    _sub.on_completed();
-        });
-
-    obs1.with_latest_from(obs2) //join values from first observable and the most recent value from second observable
-        .subscribe([](std::tuple<int, int> _v) {
-        auto [a, b] = _v;
-
-            });
-            return 0;
-
+    obs1.with_latest_from(obs2)
+        .subscribe([](auto _v) { 
+        auto [a, b] = _v;});
+    return 0;
 }
